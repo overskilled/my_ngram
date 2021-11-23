54a5e395e1679ebfe44dc83aea03f7cb7624c243
ouatedem tsafack yvan loic <yvan-loi_o@qwasar.io>
2021-11-23 01:30:47 +0000

    nice

---
+++ ex00/my_ngram.c
@@ -0,0 +1,25 @@
+#define MAX_ARRAY_SIZE 128
+
+void fill_array(int *array,int size_array, char *str){
+    printf("COMMENT STRING %s\n",str);
+    while(str1[i]!='\0'){
+        printf("%c <-> %d",str[i]);
+        i++;
+    }
+}
+
+void print_array(int* array, int size_array){
+    int i=0;
+    
+}
+
+int main(int n,char **str){
+    int i;
+
+
+    while(i<n){
+        fill_array(str[i])
+        i++;}
+    print_array();
+    return 0;
+}
