43da4b9467112de6d7522deffd09be9e98ff6bab
naudje todjo dilan wilfred <dilan-wi_n@qwasar.io>
2021-12-01 00:08:31 +0000

    DONE

--- ex00/my_ngram.c
+++ ex00/my_ngram.c
@@ -1,90 +1,29 @@
-/*
 #include <stdio.h>
-#include <stdlib.h>
 #define MAX_ARRAY_SIZE 128
-
 
 void fill_array(int* array,char* str){
-   int i=0;
-    
-    while(str[i]!='\0'){
-        if(str[i]!='"'){
-            array[(int)str[i]]+=1;
-        }
-        i+=1;
+    int i=0;
+    while(str[i] != '\0'){
+        if(str[i] != '"'){
+      array[(int)str[i]]+=1;}
+        i++;
     }
 }
-
 void print_array(int* array, int size_array){
     int i=0;
-
     while(i<size_array){
         if(array[i]>0){
-        printf("%c:%d\n",i,array[i]);
-        }
+            printf("%c:%d\n", i, array[i]);}
         i+=1;
-    }
-  
+            }
 }
-
-int main(int ac,char **av){
-    int i;
+int main(int ac, char** av){
+    int i=1;
     int array[MAX_ARRAY_SIZE]= {0};
-
     while(i<ac){
-        fill_array(&array[0],av[i]);
-        i+=1;
-        }
-  print_array(&array[0], MAX_ARRAY_SIZE);
-    return EXIT_SUCCESS;
-}
-*/
-
-/*
-
-aaa
-
-array[letter] += 1
-
-1# go throught arguments
-2# fill an array with occurence
-3# printing the array
-
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX_ARRAY_SIZE 128
-
-void fill_array(int* array, char* str) {
-    int index = 0;
-
-    while (str[index] != '\0') {
-        if (str[index] != '"') {
-            array[(int)str[index]] += 1;
-        }
-        index += 1;
+    fill_array(&array[0],av[i]);
+    i+=1;
     }
-}
-
-void print_array(int* array, int size_array) {
-    int index = 0;
-
-    while (index < size_array) {
-        if (array[index] > 0) {
-            printf("%c:%d\n", index, array[index]);
-        }
-        index += 1;
-    }
-}
-
-int main(int ac, char **av) {
-    int index = 1;
-    int array[MAX_ARRAY_SIZE] = {0};
-
-    while (index < ac) {
-        fill_array(&array[0], av[index]);
-        index += 1;
-    }
-    print_array(&array[0], MAX_ARRAY_SIZE);
-    return EXIT_SUCCESS;
+    print_array(&array[0],MAX_ARRAY_SIZE);
+    return 0;
 }
